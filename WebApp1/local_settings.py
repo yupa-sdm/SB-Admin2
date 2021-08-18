d060e5a798d84a77e1f256f8ddd81c87281d3e87
yupa-sdm <86347328+yupa-sdm@users.noreply.github.com>
2021-08-18 22:54:50 +0700

    prepare to deploy

--- WebApp1/local_settings.py
+++ WebApp1/local_settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,9 +87,6 @@ DATABASES = {
        'PORT': '',
     }
 }
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 
 # Password validation
--- web/models.py
+++ web/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
+CATEGORY_CHOICES =[
+    (1, 'ข่าวประกาศ'),
+    (2, 'โฆษณา'),
+    (3, 'บันทึกทั่วไป'),
+]
+
+PREFIX_CHOICES = [
+    (1, 'นาย'),
+    (2, 'นางสาว'),
+    (3, 'นาง'),
+]
+
 # Create your models here.
+
+
+class Blog(models.Model):
+    """Model definition for Blog."""
+
+    # TODO: Define fields here
+    title = models.CharField(max_length=255)
+    category = models.IntegerField(choices=CATEGORY_CHOICES, default=1)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Meta definition for Blog."""
+
+        verbose_name = 'Blog'
+        verbose_name_plural = 'Blogs'
+
+    def __str__(self):
+        """Unicode representation of Blog."""
+        return self.title
+
+class Author(models.Model):
+    """Model definition for Author."""
+
+    # TODO: Define fields here
+    prefix = models.IntegerField(choices=PREFIX_CHOICES)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dob = models.DateField()
+
+    class Meta:
+        """Meta definition for Author."""
+
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
+    def __str__(self):
+        """Unicode representation of Author."""
+        return self.first_name + " " + self.last_name
